548323d87b26d57166f052d865257634f51b3ea8
Farha-A <159956147+Farha-A@users.noreply.github.com>
2024-10-13 22:55:53 +0300

    Removed app.py comments

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify
-# , render_template, request
 from routes.brrp import brrp_bp
 
 app = Flask(__name__)
@@ -9,20 +8,6 @@ app.register_blueprint(brrp_bp, url_prefix="/brrp")
 @app.errorhandler(404)
 def not_found(e):
  return jsonify({"error": "Resource not found"}), 404
-
-# @app.route('/')
-# def home():
-#  return render_template('index.html')
-
-# # POST endpoint for form filling
-# @app.route('/submit', methods=['POST'])
-# def submit():
-#  name = request.form['name']
-#  id = request.form['id']
-#  age = request.form['age']
-#  mail = request.form['mail']
-#  return render_template('res.html', name=name, age=age, id=id, mail=mail)
-
 
 if __name__ == '__main__':
  app.run(debug=True)
