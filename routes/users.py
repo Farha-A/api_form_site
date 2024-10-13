070e03e959959ff52a7dd5987cbe1e3f520cefdf
Farha-A <159956147+Farha-A@users.noreply.github.com>
2024-10-13 23:11:17 +0300

    Update users.py
    Added 5th endpoint
    Updated user information saved

--- routes/users.py
+++ routes/users.py
@@ -10,7 +10,7 @@ users = []
 def bef_req():
     auth_token()
 
-# Get endpoint - get all users
+# Get endpoint 1 - get all users
 @users_bp.route("/", methods=["GET"])
 def get_users():
     return jsonify(users)
@@ -20,23 +20,38 @@ def get_users():
 def create_user():
     user = {
         "sys_id": len(users)+1,
-        "name": request.json.get("name")
+        "user_id": request.json.get("id"),
+        "name": request.json.get("name"),
+        "age": request.json.get("age"),
+        "email": request.json.get("email")
     }
     users.append(user)
     return jsonify(user), 201
 
 # Put endpoint - update existing user
-@users_bp.route("/<int:id>", methods=["PUT"])
+@users_bp.route("/<int:sys_id>", methods=["PUT"])
 def update_user(id):
-    user = next((i for i in users if i["id"] == id), None)
+    user = next((i for i in users if i["sys_id"] == id), None)
     if user is None:
         return jsonify({"error": "User not found"}), 404
     user["name"] = request.json.get("name", user["name"])
     return(jsonify(user))
 
 # Delete endpoint - remove user
-@users_bp.route("<int:id>", methods=["DELETE"])
+@users_bp.route("<int:sys_id>", methods=["DELETE"])
 def del_user(id):
     global users
-    users = [i for i in users if i["id"]!=id]
+    users = [i for i in users if i["sys_id"]!=id]
     return '', 204
+
+# Get endpoint 2 - return specific user
+@users_bp.route("<int:user_id>", methods=["GET"])
+def get_user_via_id(id):
+    user = next((i for i in users if i["user_id"] == id), None)
+    if user is None:
+        return jsonify({"error": "User not found"}), 404
+    user["sys_id"] = request.json.get("sys_id", user["sys_id"])
+    user["name"] = request.json.get("name", user["name"])
+    user["age"] = request.json.get("age", user["age"])
+    user["email"] = request.json.get("email", user["email"])
+    return(jsonify(user))
