3da8b0ee2138de95cf39ca9c7f52db413c15836e
Farha-A <159956147+Farha-A@users.noreply.github.com>
2024-10-14 20:23:45 +0300

    Update users.py

--- routes/users.py
+++ routes/users.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from middleware.auth import auth_token
+from middleware.auth import authenticate_token
 
 users_bp = Blueprint("users", __name__)
 
@@ -7,8 +7,8 @@ users = []
 
 # middleware authentication
 @users_bp.before_request
-def bef_req():
-    auth_token()
+def before_request():
+    authenticate_token()
 
 # Get endpoint 1 - get all users
 @users_bp.route("/", methods=["GET"])
@@ -19,8 +19,8 @@ def get_users():
 @users_bp.route("/", methods=["POST"])
 def create_user():
     user = {
-        "sys_id": len(users)+1,
-        "user_id": request.json.get("id"),
+        "id": len(users)+1,
+        "user_id": request.json.get("user_id"),
         "name": request.json.get("name"),
         "age": request.json.get("age"),
         "email": request.json.get("email")
@@ -29,29 +29,25 @@ def create_user():
     return jsonify(user), 201
 
 # Put endpoint - update existing user
-@users_bp.route("/<int:sys_id>", methods=["PUT"])
+@users_bp.route("/<int:id>", methods=["PUT"])
 def update_user(id):
-    user = next((i for i in users if i["sys_id"] == id), None)
+    user = next((i for i in users if i["id"] == id), None)
     if user is None:
         return jsonify({"error": "User not found"}), 404
     user["name"] = request.json.get("name", user["name"])
     return(jsonify(user))
 
 # Delete endpoint - remove user
-@users_bp.route("<int:sys_id>", methods=["DELETE"])
+@users_bp.route("<int:id>", methods=["DELETE"])
 def del_user(id):
     global users
-    users = [i for i in users if i["sys_id"]!=id]
+    users = [i for i in users if i["id"]!=id]
     return '', 204
 
 # Get endpoint 2 - return specific user
-@users_bp.route("<int:user_id>", methods=["GET"])
-def get_user_via_id(id):
-    user = next((i for i in users if i["user_id"] == id), None)
+@users_bp.route("/<int:user_id>", methods=["GET"])
+def get_user_via_id(user_id):
+    user = next((i for i in users if i["user_id"] == user_id), None)
     if user is None:
         return jsonify({"error": "User not found"}), 404
-    user["sys_id"] = request.json.get("sys_id", user["sys_id"])
-    user["name"] = request.json.get("name", user["name"])
-    user["age"] = request.json.get("age", user["age"])
-    user["email"] = request.json.get("email", user["email"])
     return(jsonify(user))
